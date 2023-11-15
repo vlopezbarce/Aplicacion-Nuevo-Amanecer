b08680d168ad1e1494a3485f16d638c3d40ffd75
Martha Mendoza A01284654 <mmarthamendozaa@gmail.com>
2023-11-15 13:23:56 -0600

    añadi kt y xml y nav_graph de alumnofragments 2-4

---
+++ app/src/main/java/com/tec/nuevoamanecer/AlumnoFragment2.kt
@@ -0,0 +1,73 @@
+package com.tec.nuevoamanecer
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.tec.nuevoamanecer.databinding.FragmentAlumno2Binding
+
+class AlumnoFragment2 : Fragment() {
+    private var _binding : FragmentAlumno2Binding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var database: DatabaseReference
+    private lateinit var userRef: DatabaseReference
+    private lateinit var userUID: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        database = FirebaseDatabase.getInstance().reference
+        userUID = arguments?.getString("userUID").orEmpty()
+        userRef = database.child("Usuarios").child("Alumnos").child(userUID)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAlumno2Binding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        userRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    val alumno = snapshot.getValue(Alumno::class.java)
+                    if (alumno != null) {
+                        binding.txtViewNombre.text = alumno.nombre
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+
+        binding.btnRegresar.setOnClickListener{
+            Navigation.findNavController(view).navigate(R.id.action_alumnoFragment2_to_mainFragment)
+        }
+
+
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            AlumnoFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
